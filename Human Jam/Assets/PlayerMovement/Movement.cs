1228a18a92742be6a9197f33bfca27bd785ba692
jxisconfused <44506722+jxisconfused@users.noreply.github.com>
2019-05-12 18:32:57 +0800

    PlayerMovement
    just a tryout to see if it works

---
+++ Human Jam/Assets/PlayerMovement/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float ForwardSpeed = 500f;
+    public float LRSpeed = 1000f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       // rb.AddForce(0, 0, 200 * Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-LRSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(LRSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
---
+++ Human Jam/Assets/PlayerMovement/PlayerGravity.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class PlayerGravity : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
