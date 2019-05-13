9bf3b3bf45ff72d24b8e43a3221f2ba07d84d060
jxisconfused <44506722+jxisconfused@users.noreply.github.com>
2019-05-13 23:23:37 +0800

    Added dummy model
    Dummy model for testing collision.

--- Human Jam/Assets/PlayerMovement/PlayerMovement.cs
+++ Human Jam/Assets/PlayerMovement/PlayerMovement.cs
@@ -3,21 +3,31 @@
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
-    public float ForwardSpeed = 500f;
-    public float LRSpeed = 1000f;
+    public float ForwardSpeed = 5000f;
+    public float LRSpeed = 10000f;
     // Update is called once per frame
     void FixedUpdate()
     {
-       // rb.AddForce(0, 0, 200 * Time.deltaTime);
+        rb.AddForce(0, 0, 200 * Time.deltaTime);
 
         if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             rb.AddForce(-LRSpeed * Time.deltaTime, 0, 0);
         }
-
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+       
+        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             rb.AddForce(LRSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(0, 0 , ForwardSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(0, 0, -ForwardSpeed * Time.deltaTime);
         }
     }
 }
