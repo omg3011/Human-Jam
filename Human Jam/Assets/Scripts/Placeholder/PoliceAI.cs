2c958db0c3574c954f72fce3b30389f0a821de5d
Darren <darrenlow0@gmail.com>
2019-05-14 11:16:48 +0800

    fixed bug

--- Human Jam/Assets/Scripts/Placeholder/PoliceAI.cs
+++ Human Jam/Assets/Scripts/Placeholder/PoliceAI.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AI : MonoBehaviour
+public class PoliceAI : MonoBehaviour
 {
-    private Transform T_player;
-    private float speed = 10.0f;
+    public GameObject T_player;
+    public float speed = 10.0f;
+    private Vector3 moveDir;
     // Start is called before the first frame update
     void Start()
     {
-      
+        T_player = GameObject.FindWithTag("Player");
+        moveDir = (T_player.transform.position - transform.position).normalized;
+        moveDir.y = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveDir = (T_player.position - transform.position).normalized;
-        moveDir.y = 0;
-
         transform.position += moveDir * speed * Time.deltaTime;
     }
 }
